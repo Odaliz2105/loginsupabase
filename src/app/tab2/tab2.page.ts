0868dc9a30b99048decb8da0e8f6c6e3126dc495
Odaliz2105 <balsecaodaliz@gmail.com>
2026-05-07 15:50:35 -0500

    Proyecto Ionic CV con Supabase

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+
+import { addIcons } from 'ionicons';
+
+import {
+  school,
+  laptop,
+  business,
+  code,
+  construct,
+  analytics,
+  ribbon
+} from 'ionicons/icons';
+
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonAvatar,
+  IonText,
+  IonChip,
+  IonIcon,
+  IonLabel,
+  IonList,
+  IonItem,
+  IonListHeader,
+  IonProgressBar,
+  IonButton
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
+  standalone: true,
+
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonAvatar,
+    IonText,
+    IonChip,
+    IonIcon,
+    IonLabel,
+    IonList,
+    IonItem,
+    IonListHeader,
+    IonProgressBar,
+    IonButton
+  ]
+})
+export class Tab2Page {
+
+  constructor() {
+
+    addIcons({
+      school,
+      laptop,
+      business,
+      code,
+      construct,
+      analytics,
+      ribbon
+    });
+
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://tcjalkrykukljhzqccfo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjamFsa3J5a3VrbGpoenFjY2ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzY0NTIsImV4cCI6MjA5Mzc1MjQ1Mn0.JXyAOjhsxi1hS6MGHJTV7YSd23_xFaq9nUnZFY94pLE'
+};
